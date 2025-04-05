e54aa9d089416e9d1f3940293f2f510f267c43b1
GetaendaleG <GetaendaleG@gmail.com>
2025-04-05 22:24:53 +0300

    add temprature and location section

--- react-weather-app/src/Components/Weather.jsx
+++ react-weather-app/src/Components/Weather.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import search_icon from '../assets/search.png'
 import './weather.css'
+import clear_icon from '../assets/clear.png'
 const Weather = () => {
   return (
     <div className='weather'>
@@ -8,6 +9,9 @@ const Weather = () => {
             <input type="text" placeholder='Search' />
             <img className='search-icon' src={search_icon} alt="search icon" />
         </div>
+        <img src={clear_icon} alt="clear weather icon" className="weather-icon" />
+        <p className='temprature'>16°c</p>
+        <p className='location'>London</p>
     </div>
   )
 }
