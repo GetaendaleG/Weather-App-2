962acbf35ec0177db6e4826751730301037f411a
GetaendaleG <GetaendaleG@gmail.com>
2025-04-07 21:26:32 +0300

    finished the ui part

--- react-weather-app/src/Components/Weather.jsx
+++ react-weather-app/src/Components/Weather.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import search_icon from '../assets/search.png'
 import './weather.css'
 import clear_icon from '../assets/clear.png'
+import humidity_icon from '../assets/humidity.png'
+import wind_icon from '../assets/wind.png'
 const Weather = () => {
+    const search = async (city)=>{
+        try{
+            const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
+        }catch(error){
+
+        }
+    }
+
+
   return (
     <div className='weather'>
         <div className="search">
@@ -12,6 +23,22 @@ const Weather = () => {
         <img src={clear_icon} alt="clear weather icon" className="weather-icon" />
         <p className='temprature'>16°c</p>
         <p className='location'>London</p>
+        <div className="weather-data">
+            <div className="col">
+                <img src={humidity_icon} alt="" />
+                <div className='inner-div'>
+                    <p>91%</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+            <div className="col">
+                <img src={wind_icon} alt="humidity icon" />
+                <div className='inner-div'>
+                    <p>3.6 Km/hr</p>
+                    <span>Wind Speed</span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
