ad41fda3ff1e945e18c2329481303b0ec45251c8
GetaendaleG <GetaendaleG@gmail.com>
2025-04-09 02:35:17 +0300

    add search functionality and updated weather.css

--- react-weather-app/src/Components/Weather.jsx
+++ react-weather-app/src/Components/Weather.jsx
@@ -1,40 +1,81 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import search_icon from '../assets/search.png'
 import './weather.css'
 import clear_icon from '../assets/clear.png'
 import humidity_icon from '../assets/humidity.png'
 import wind_icon from '../assets/wind.png'
 const Weather = () => {
+    const [weatherData,setWeatherData] = useState(false);
+    const inputRef  = useRef();
     const search = async (city)=>{
+        if(city===""){
+            alert('Enter city Name');
+            return;
+        }
         try{
-            const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
-        }catch(error){
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
+
+            const response = await fetch(url);
+            const data  = await response.json();
+            console.log(data);
+            
+            setWeatherData(
+                {
+                    humidity: data.main.humidity,
+                    wind: data.wind.speed,
+                    tempreture: Math.floor(data.main.temp),
+                    location: data.name,
+                    icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+                }
+            )
+        }catch(error){
+            console.error("Error fetching weather data:", error);
+            alert("Failed to fetch weather data. Please try again.");
         }
     }
-
+    useEffect(()=>{
+        search("dire dawa")
+    },[])
 
   return (
     <div className='weather'>
         <div className="search">
-            <input type="text" placeholder='Search' />
-            <img className='search-icon' src={search_icon} alt="search icon" />
+            <input 
+                ref={inputRef} 
+                type="text" 
+                placeholder="Search" 
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        search(inputRef.current.value);
+                    }
+                }} 
+            />
+            <img 
+                className="search-icon" 
+                onClick={() => {
+                    search(inputRef.current.value);
+                }} 
+                src={search_icon} 
+                alt="search icon" 
+            />
         </div>
-        <img src={clear_icon} alt="clear weather icon" className="weather-icon" />
-        <p className='temprature'>16°c</p>
-        <p className='location'>London</p>
+        <img src={weatherData.icon} alt="clear weather icon" className="weather-icon" />
+        <p className='temprature'>{weatherData.tempreture}°c</p>
+        <p className='location'>{weatherData.location}</p>
         <div className="weather-data">
             <div className="col">
                 <img src={humidity_icon} alt="" />
                 <div className='inner-div'>
-                    <p>91%</p>
+                    <p>{weatherData.humidity}%</p>
                     <span>Humidity</span>
                 </div>
             </div>
             <div className="col">
                 <img src={wind_icon} alt="humidity icon" />
                 <div className='inner-div'>
-                    <p>3.6 Km/hr</p>
+                    <p>{weatherData.wind} Km/hr</p>
                     <span>Wind Speed</span>
                 </div>
             </div>
